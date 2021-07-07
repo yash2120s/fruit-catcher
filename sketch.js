abd4756f18eaf6c5d809bb60c2f0071b73df7d8a
yash2120s <80688710+yash2120s@users.noreply.github.com>
2021-07-07 08:15:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,13 +14,13 @@ var player_img;
 var score1,score2;
 
 function preload(){
-  back_img = loadImage("images/jungle.jpg");
-  player_img = loadImage("images/basket2.png");
-  fruit1_img = loadImage("images/apple2.png");
-  fruit2_img = loadImage("images/banana2.png");
-  fruit3_img = loadImage("images/melon2.png");
-  fruit4_img = loadImage("images/orange2.png");
-  fruit5_img = loadImage("images/pineapple2.png");
+  back_img = loadImage("jungle.jpg");
+  player_img = loadImage("basket2.png");
+  fruit1_img = loadImage("apple2.png");
+  fruit2_img = loadImage("banana2.png");
+  fruit3_img = loadImage("melon2.png");
+  fruit4_img = loadImage("orange2.png");
+  fruit5_img = loadImage("pineapple2.png");
   fruitGroup = new Group();
 }
 function setup() {
